354bade05127e33058876c1e28d3ac7dcc6f204f
Techmad <techmad60@gmail.com>
2024-10-31 00:45:36 -0700

    Terms Of Service Finished

--- app/terms-of-service/page.tsx
+++ app/terms-of-service/page.tsx
@@ -1,65 +1,51 @@
 //app/terms-of-service/page.tsx
 import Header from "@/components/sections/Header";
 import Footer from "@/components/sections/Footer";
-import { montsy } from "../fonts/fonts";
-import { poppins } from "../fonts/fonts";
 import DownloadApp from "@/components/ui/DownloadApp";
 
 
 export default function TermsOfService() {
     return (
-        <div className={`${montsy.className} flex flex-col`}>
+        <div className={`flex flex-col`}>
             <Header/>
-            <main>
-                <section className="flex flex-col justify-center items-center pt-16">
-                    <h1 className={`text-color-zero font-semibold text-xl my-4 text-center lg:text-4xl  ${poppins.className}`}>
-                        Terms of Service
-                    </h1>
-                    <p className="text-sm text-color-zero lg:text-xl">
-                    Updated Last: 30th September 2024
-                    </p>
-                </section>
-                <section className="flex flex-col justify-center items-center max-w-5xl mx-auto">
-                    <p className="px-12 text-xs text-color-zero font-extralight lg:text-sm pt-8">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <p className="px-12 text-xs text-color-zero font-light lg:text-sm pt-8">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <p className="px-12 text-xs text-color-zero font-light lg:text-sm pt-8">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <p className="px-12 text-xs text-color-zero font-light lg:text-sm pt-8">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <p className="px-12 text-xs text-color-zero font-light lg:text-sm pt-8">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <p className="px-12 text-xs text-color-zero font-light lg:text-sm pt-8">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <p className="px-12 text-xs text-color-zero font-light lg:text-sm pt-8">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <p className="px-12 text-xs text-color-zero font-light lg:text-sm pt-8">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <p className="px-12 text-xs text-color-zero font-light lg:text-sm pt-8">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <p className="px-12 text-xs text-color-zero font-light lg:text-sm pt-8">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <p className="px-12 text-xs text-color-zero font-light lg:text-sm pt-8">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                    <p className="px-12 text-xs text-color-zero font-light lg:text-sm pt-8">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                    </p>
-                </section>
-                <section className="flex flex-col justify-center items-center bg-color-two py-16">
-                    <DownloadApp />
-                </section>
+            <main className="">
+                <div className="flex flex-col px-4 mx-auto lg:max-w-[57.5rem] xl:max-w-[75.5rem] ">
+                    <section className="flex flex-col justify-start items-start mt-32">
+                        <h1 className={`text-color-zero text-lg-base font-extrabold my-4 text-start lg:text-[39px]`}>
+                            Terms Of Service
+                        </h1>
+                        <p className="text-sm text-colour-five lg:text-base">
+                        Updated Last: 30th September 2024
+                        </p>
+                    </section>
+                    <section className="flex flex-col justify-start items-start space-y-6 py-8 lg:space-y-20">
+                        <p className="text-sm text-colour-five leading-[28px] lg:text-lg lg:leading-[44px]">
+                            Elit at et amet dis. Tortor in lacus eget purus at malesuada nibh scelerisque. Eu in neque vitae aenean. Commodo maecenas placerat consectetur diam leo. Tristique porttitor nec tristique est varius felis. Ligula donec fames auctor leo pretium lectus. Pulvinar non enim nisi cursus dictum enim sagittis nunc a. A interdum dolor aliquet lacus sagittis. Nisl vestibulum pretium id condimentum at quam vitae eros sed. Sit eget quam mattis sapien massa. Et quisque morbi erat tellus donec urna lorem tellus. In pharetra commodo velit tincidunt. Laoreet consequat diam quis lectus lectus turpis vitae tortor faucibus. Odio quis eleifend vehicula porttitor vitae sit.
+                        </p>
+                        <p className="text-sm text-colour-five leading-[28px] lg:text-lg lg:leading-[44px]">
+                            Elit at et amet dis. Tortor in lacus eget purus at malesuada nibh scelerisque. Eu in neque vitae aenean. Commodo maecenas placerat consectetur diam leo. Tristique porttitor nec tristique est varius felis. Ligula donec fames auctor leo pretium lectus. Pulvinar non enim nisi cursus dictum enim sagittis nunc a. A interdum dolor aliquet lacus sagittis. Nisl vestibulum pretium id condimentum at quam vitae eros sed. Sit eget quam mattis sapien massa. Et quisque morbi erat tellus donec urna lorem tellus. In pharetra commodo velit tincidunt. Laoreet consequat diam quis lectus lectus turpis vitae tortor faucibus. Odio quis eleifend vehicula porttitor vitae sit.
+                        </p>
+                        <p className="text-sm text-colour-five leading-[28px] lg:text-lg lg:leading-[44px]">
+                            Elit at et amet dis. Tortor in lacus eget purus at malesuada nibh scelerisque. Eu in neque vitae aenean. Commodo maecenas placerat consectetur diam leo. Tristique porttitor nec tristique est varius felis. Ligula donec fames auctor leo pretium lectus. Pulvinar non enim nisi cursus dictum enim sagittis nunc a. A interdum dolor aliquet lacus sagittis. Nisl vestibulum pretium id condimentum at quam vitae eros sed. Sit eget quam mattis sapien massa. Et quisque morbi erat tellus donec urna lorem tellus. In pharetra commodo velit tincidunt. Laoreet consequat diam quis lectus lectus turpis vitae tortor faucibus. Odio quis eleifend vehicula porttitor vitae sit.
+                        </p>
+                        <p className="text-sm text-colour-five leading-[28px] lg:text-lg lg:leading-[44px]">
+                            Elit at et amet dis. Tortor in lacus eget purus at malesuada nibh scelerisque. Eu in neque vitae aenean. Commodo maecenas placerat consectetur diam leo. Tristique porttitor nec tristique est varius felis. Ligula donec fames auctor leo pretium lectus. Pulvinar non enim nisi cursus dictum enim sagittis nunc a. A interdum dolor aliquet lacus sagittis. Nisl vestibulum pretium id condimentum at quam vitae eros sed. Sit eget quam mattis sapien massa. Et quisque morbi erat tellus donec urna lorem tellus. In pharetra commodo velit tincidunt. Laoreet consequat diam quis lectus lectus turpis vitae tortor faucibus. Odio quis eleifend vehicula porttitor vitae sit.
+                        </p>
+                        <p className="text-sm text-colour-five leading-[28px] lg:text-lg lg:leading-[44px]">
+                            Elit at et amet dis. Tortor in lacus eget purus at malesuada nibh scelerisque. Eu in neque vitae aenean. Commodo maecenas placerat consectetur diam leo. Tristique porttitor nec tristique est varius felis. Ligula donec fames auctor leo pretium lectus. Pulvinar non enim nisi cursus dictum enim sagittis nunc a. A interdum dolor aliquet lacus sagittis. Nisl vestibulum pretium id condimentum at quam vitae eros sed. Sit eget quam mattis sapien massa. Et quisque morbi erat tellus donec urna lorem tellus. In pharetra commodo velit tincidunt. Laoreet consequat diam quis lectus lectus turpis vitae tortor faucibus. Odio quis eleifend vehicula porttitor vitae sit.
+                        </p>
+                        <p className="text-sm text-colour-five leading-[28px] lg:text-lg lg:leading-[44px]">
+                            Elit at et amet dis. Tortor in lacus eget purus at malesuada nibh scelerisque. Eu in neque vitae aenean. Commodo maecenas placerat consectetur diam leo. Tristique porttitor nec tristique est varius felis. Ligula donec fames auctor leo pretium lectus. Pulvinar non enim nisi cursus dictum enim sagittis nunc a. A interdum dolor aliquet lacus sagittis. Nisl vestibulum pretium id condimentum at quam vitae eros sed. Sit eget quam mattis sapien massa. Et quisque morbi erat tellus donec urna lorem tellus. In pharetra commodo velit tincidunt. Laoreet consequat diam quis lectus lectus turpis vitae tortor faucibus. Odio quis eleifend vehicula porttitor vitae sit.
+                        </p>
+                        <p className="text-sm text-colour-five leading-[28px] lg:text-lg lg:leading-[44px]">
+                            Elit at et amet dis. Tortor in lacus eget purus at malesuada nibh scelerisque. Eu in neque vitae aenean. Commodo maecenas placerat consectetur diam leo. Tristique porttitor nec tristique est varius felis. Ligula donec fames auctor leo pretium lectus. Pulvinar non enim nisi cursus dictum enim sagittis nunc a. A interdum dolor aliquet lacus sagittis. Nisl vestibulum pretium id condimentum at quam vitae eros sed. Sit eget quam mattis sapien massa. Et quisque morbi erat tellus donec urna lorem tellus. In pharetra commodo velit tincidunt. Laoreet consequat diam quis lectus lectus turpis vitae tortor faucibus. Odio quis eleifend vehicula porttitor vitae sit.
+                        </p>
+                        <p className="text-sm text-colour-five leading-[28px] lg:text-lg lg:leading-[44px]">
+                            Elit at et amet dis. Tortor in lacus eget purus at malesuada nibh scelerisque. Eu in neque vitae aenean. Commodo maecenas placerat consectetur diam leo. Tristique porttitor nec tristique est varius felis. Ligula donec fames auctor leo pretium lectus. Pulvinar non enim nisi cursus dictum enim sagittis nunc a. A interdum dolor aliquet lacus sagittis. Nisl vestibulum pretium id condimentum at quam vitae eros sed. Sit eget quam mattis sapien massa. Et quisque morbi erat tellus donec urna lorem tellus. In pharetra commodo velit tincidunt. Laoreet consequat diam quis lectus lectus turpis vitae tortor faucibus. Odio quis eleifend vehicula porttitor vitae sit.
+                        </p>
+                    </section>
+                </div>
+                <DownloadApp />
             </main>
             <Footer/>
         </div>
