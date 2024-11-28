b6e96754bb15aeb9d772c2d5d89b2c48d74bfc0d
Techmad <techmad60@gmail.com>
2024-11-27 21:16:20 -0800

    victory

--- app/main/dashboard/layout.tsx
+++ app/main/dashboard/layout.tsx
@@ -15,10 +15,10 @@ export default function DashboardLayout({
   return (
     <div className="flex flex-col bg-white">
      
-      <div className={`p-4 lg:grid lg:p-0 lg:grid-cols-3 xl:grid-cols-5`}>
+      <div className={`p-4 lg:grid lg:p-0 lg:grid-cols-4 xl:grid-cols-5 xl:gap-44 2xlg:gap-0`}>
         <MainPageNavbar isNavOpen={isNavOpen} toggleNav={toggleNav} />
         {/* Item 2 */}
-        <div className="flex flex-col lg:col-span-2 xl:col-span-4">
+        <div className="flex flex-col lg:col-span-3 xl:col-span-4">
           <AuthHeader title="Dashboard" toggleNav={toggleNav} grid=""/>
           {children} {/* Ensure children are rendered here */}
         </div>
