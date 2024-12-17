e8bffa328c013fed56ea4067b681a393627ae9b3
Techmad <techmad60@gmail.com>
2024-12-16 21:24:51 -0800

    next

--- app/main/dashboard/page.tsx
+++ app/main/dashboard/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import Link from "next/link";
 import { TbTargetArrow } from "react-icons/tb";
 import { BsFileBarGraphFill } from "react-icons/bs";
