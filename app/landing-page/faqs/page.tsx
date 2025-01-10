18543a4aa718d39454e9c696c272826feb34de24
Techmad <techmad60@gmail.com>
2025-01-10 05:29:28 -0800

    funny

--- app/landing-page/faqs/page.tsx
+++ app/landing-page/faqs/page.tsx
@@ -1,9 +1,9 @@
 "use client"
-import Header from "@/components/Sections/Header";
-import Footer from "@/components/Sections/Footer";
 import DownloadApp from "@/components/ui/DownloadApp";
 import { useState } from "react";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import Footer from "../../../components/Sections/Footer";
+import Header from '../../../components/Sections/Header';
 
 export default function Faqs() {
     const [openIndex, setOpenIndex] = useState<number | null>(null);
